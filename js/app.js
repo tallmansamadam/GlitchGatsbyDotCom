848d22485c1f82aac5415b449d548b7835a425e2
tallmansamadam <tallman.samadam@gmail.com>
2022-10-27 21:20:33 -0500

    Mobile Hover Fix attempt 3

--- js/app.js
+++ js/app.js
@@ -23,41 +23,23 @@ date.innerHTML = new Date().getFullYear();
 
 
 
-let options = {
-  root: null, // relative to document viewport
-  rootMargin: "0px", // margin around root. Values are similar to css property. Unitless values not allowed
-  threshold: .75, // visible amount of item shown in relation to root
-};
 
-const projectCard = document.querySelectorAll(".project");
-const projectImage = document.querySelectorAll(".project-img");
-projectCard.forEach((project) => observer.observe(project, {}));
-projectImage.forEach((projectimg) => observer2.observe(projectimg, {}));
-
-
-let observer = new IntersectionObserver((entries) => {
+const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     entry.target.classList.toggle("project-mobile-hover", entry.isIntersecting)
     ;
-}, options)
-
-let observer2 = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    entry.target.classList.toggle("mobile-hover", entry.isIntersecting);
-    // Element.classList.add(project-)
-  });
-});
+})
 // mobile hover work-around for project class
 // const observer = new IntersectionObserver(
 //     // Element.classList.add(project-)
 //   });
-// // );
-// document
-//   .querySelectorAll(".project")
-//   .forEach((project) => observer.observe(project, {}));
+// );
+document
+  .querySelectorAll(".project")
+  .forEach((project) => observer.observe(project, {}));
 
-// mobile hover work-around for project-img class
-// let observer2 = new IntersectionObserver((entries) => {
+// // mobile hover work-around for project-img class
+// let observer2 = new IntersectionObserver((entries2) => {
 //   entries.forEach((entry) => {
 //     entry.target.classList.toggle("mobile-hover", entry.isIntersecting);
 //     // Element.classList.add(project-)
